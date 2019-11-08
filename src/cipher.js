927f3655de4041fda24acc924b420dca438c99d4
Didre <ceci21062@gmail.com>
2019-11-08 10:14:52 -0600

    entrega final

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,57 @@
 window.cipher = {
   encode: (uppercaseString, displacement) => {
-    let replace = "";
-
-    for (let i = 0; i < uppercaseString.length; i++) {
-      let formula = (uppercaseString.charCodeAt(i) - 65 + displacement) % 26 + 65;
-      replace += String.fromCharCode(formula);
+  let replace = "";
+  for (let i = 0; i < uppercaseString.length; i++) {
+    let asciiMsg = uppercaseString.charCodeAt(i);
+    if (65 <= asciiMsg && asciiMsg <= 90) {
+      replace += String.fromCharCode(
+        ((asciiMsg - 65 + parseInt(displacement)) %26) + 65
+      );
+    } else if (97 <= asciiMsg && asciiMsg <= 122){
+      replace += String.fromCharCode(
+        ((asciiMsg - 97 + parseInt(displacement)) % 26) +97
+      );
+    } else {
+      replace += uppercaseString.charAt(i);
+      }
     }
-
     return replace;
+  
   },
-
   decode: (displace, des) => {
-
-    let rpl = "";
-    for (let i = 0; i < des.length; i++) {
-      let txto = des.charCodeAt(i); {
-        rpl += String.fromCharCode(
-          ((txto + 65 - parseInt(displace)) % 26) + 65);
+  let rpl = "";
+  for (let i=0; i < des.length; i++) {
+    let asciiMsg = des.charCodeAt(i);
+    if (65 <= asciiMsg && asciiMsg <= 90) {
+      rpl += String.fromCharCode(
+        ((asciiMsg + 65 - parseInt(displace)) % 26) + 65
+      );
+    } else if (97 <= asciiMsg && asciiMsg <= 122){
+      rpl += String.fromCharCode(
+        ((asciiMsg - 97 - parseInt(displace)) % 26 ) + 97
+      );
+    } else {
+      rpl += des.charAt(i);
       }
     }
     return rpl;
   }
-};
+    };
+            /*
+            if (uppercaseString.charCodeAt(i) == 32) { formula = uppercaseString.charCodeAt(i)
+            } else { 
+              formula = (uppercaseString.charCodeAt(i) - 65 + displacement) % 26 + 65;
+            }
+            replace += String.fromCharCode(formula);*/
+            
+            /*decode: (displace, des) => {
+              let rpl = "";
+              for (let i = 0; i < des.length; i++) {
+                if (displace.charCodeAt(i) == 32 ) { txto = displace.charCodeAt(i)
+                } else {
+                  txto = des.charCodeAt(i); 
+                }
+                rpl += String.fromCharCode(((txto + 65 - displace)) % 26) + 65);
+              }
+              return rpl;
+            } */
--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@
         let displacement = parseInt(document.getElementById("offset").value);
    
         document.getElementById("Finaltext").value = window.cipher.encode(uppercaseLetters , displacement);
-    };
+    }
     
     document.getElementById("Descifrar").addEventListener("click", decode);
     
     function decode () {    
-        let des = document.getElementById("Finaltext").value;
+        let des = document.getElementById("Textconvert").value;
         let displace = document.getElementById("offset").value;
         
         document.getElementById("Finaltext").value = window.cipher.decode (displace , des);
-    };
+    }
     document.getElementById("Limpiar").addEventListener("click", () => {
         window.location.reload();
     });
